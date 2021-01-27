ad67a5160053e11ce933cc80f33d7cac3fcc53cd
Mika Cuvillier <mika.cuvillier@gmail.com>
2021-01-27 15:03:27 -0500

    Première utilisation de variable dans JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const nom ="React";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Allo Monde</h2>
+        <h2>Allo {nom.toUpperCase()}</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
