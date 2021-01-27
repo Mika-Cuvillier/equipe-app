db8db70abc56f6ea2a1be59d47ad1195f095f986
Mika Cuvillier <mika.cuvillier@gmail.com>
2021-01-27 15:26:13 -0500

    Supression lien

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ const nom ="React";
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Allo {nom.toUpperCase()}</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
